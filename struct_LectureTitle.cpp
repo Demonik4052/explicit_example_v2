d4b2cdabd3a871977c6e7a718df9714bce923a0e
Demonik4052 <Demonik4053@gmail.com>
2020-06-22 18:16:28 +0700

    Create struct_LectureTitle.cpp

---
+++ struct_LectureTitle.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+struct Specialization{
+	string value;
+	explicit Specialization(const string& new_value){
+		value = new_value;
+	}
+};
+
+struct Course{
+	string value;
+	explicit Course(const string& new_value){
+		value = new_value;
+	}
+};
+
+struct Week{
+	string value;
+	explicit Week(const string& new_value){
+		value = new_value;
+	}
+};
+
+struct LectureTitle{
+	string specialization;
+	string course;
+	string week;
+	explicit LectureTitle(const Specialization& new_specialization, const Course& new_course, const Week& new_week)
+	{
+		specialization = new_specialization.value;
+		course = new_course.value;
+		week = new_week.value;
+	}
+};
+
+void print(LectureTitle& title)
+{
+	cout << title.specialization << " - " << title.course << " - " << title.week << endl;
+}
+
+int main() {
+	LectureTitle title(		//it works
+		Specialization("C++"),
+		Course("White belt"),
+		Week("4th")
+	);
+
+	/* This does not work
+	 * LectureTitle title("C++", "White belt", "4th");
+	 * LectureTitle title(string("C++"), string("White belt"), string("4th"));
+	 * LectureTitle title = {"C++", "White belt", "4th"};
+	 * LectureTitle title = {{"C++"}, {"White belt"}, {"4th"}};
+	 * LectureTitle title(
+		   Course("White belt"),
+		   Specialization("C++"),
+		   Week("4th")
+	   );
+
+		LectureTitle title(
+    	   Specialization("C++"),
+    	   Week("4th"),
+       	   Course("White belt")
+	   );
+	 */
+	print(title);
+	return 0;
+}
